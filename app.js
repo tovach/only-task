571f0ebd5e5333274b7922b9482ce8127f55938c
tovach <red5.dalv@gmail.com>
2022-07-13 20:15:18 +0300

    feat: add basic functionality

---
+++ app.js
@@ -0,0 +1,28 @@
+const dom = {
+  items: document.querySelectorAll(".item"),
+  texts: document.querySelectorAll(".item__text"),
+};
+
+const hideItems = () => {
+  dom.items.forEach((item) => {
+    item.classList.remove("item--active");
+  });
+};
+
+const showItem = (e) => {
+  console.log(e.currentTarget);
+  if (e.currentTarget.classList.contains("item--active")) {
+    hideItems();
+    return;
+  }
+  hideItems();
+  e.currentTarget.classList.toggle("item--active");
+};
+
+dom.items.forEach((item) => {
+  item.addEventListener("click", showItem);
+});
+
+window.addEventListener("DOMContentLoaded", () => {
+  hideItems();
+});
